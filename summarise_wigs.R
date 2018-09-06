ffdba41c3bc6574d92af6fe2b5701e9cbab6fef5
Stefan Dentro <sd11@sanger.ac.uk>
2018-09-06 16:24:39 +0100

    Optimising the summarisation script

--- summarise_wigs.R
+++ summarise_wigs.R
@@ -5,28 +5,45 @@ args = commandArgs(T)
 stepsize = as.numeric(args[1]) 
 inputdir = args[2]
 outfile = args[3]
+organism = args[4]
+num_cores = as.numeric(args[5])
+
+if (organism=="human") {
+	chroms = paste0("chr", c(1:22, "X", "Y"))
+} else {
+	chroms = paste0("chr", c(1:19, "X", "Y"))
+}
 
 
 library(GenomicRanges)
+library(parallel)
+
 output = data.frame()
 infiles = list.files(inputdir, pattern=".wig", full.names=T)
 infiles = infiles[!grepl("unmapped", infiles)]
 print(infiles)
 dat = lapply(infiles, read.table, header=F, stringsAsFactors=F)
-for (chr in unique(dat[[1]]$V1)) {
+dat = lapply(dat, function(x) { x[x[,1] %in% chroms,] })
+
+summarise_chromosome = function(dat, chr, stepsize) {
 	print(chr)
 	len = max(dat[[1]]$V3[dat[[1]]$V1==chr])
 	intervals = data.frame(chromosome=chr, start=seq(1, (len-stepsize), stepsize), end=seq(stepsize, len, stepsize))
+	intervals_gr = makeGRangesFromDataFrame(intervals)
 	for (i in 1:length(dat)) {
 		print(i)
-		overlap = findOverlaps(makeGRangesFromDataFrame(intervals), makeGRangesFromDataFrame(dat[[i]], seqnames="V1", start.field="V2", end.field="V3", keep.extra.columns=T))
+		overlap = findOverlaps(intervals_gr, makeGRangesFromDataFrame(dat[[i]], seqnames="V1", start.field="V2", end.field="V3", keep.extra.columns=T))
 		intervals[,paste0("sample", i)] = NA
 		for (index in unique(queryHits(overlap))) {
 			intervals[index,paste0("sample", i)] = mean(dat[[i]]$V4[subjectHits(overlap)[queryHits(overlap)==index]])
 		}
 	}
-	output = rbind(output, intervals)
+	return(intervals)
 }
+
+res = mclapply(unique(dat[[1]]$V1), summarise_chromosome, dat=dat, stepsize=stepsize, mc.cores=num_cores)
+output = do.call(rbind, res)
+
 output$average = rowMeans(output[,grepl("sample", colnames(output))], na.rm=T)
 output$average[is.nan(output$average)] = NA
 output$start <-format(output$start, scientific = FALSE)
